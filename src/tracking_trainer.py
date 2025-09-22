87dc2d6dbd9bf7242417e217c9ded0a3e40cfa30
JavierZhao <zihanzhao0117@gmail.com>
2025-09-22 12:28:43 -0700

    updated naming of log dir

--- src/tracking_trainer.py
+++ src/tracking_trainer.py
@@ -223,7 +223,8 @@ def run_one_seed(config):
     if sort_type == "morton":
         sort_bits = config.get("model_kwargs", {}).get("morton_bits", 10)
         sort_suffix += f"-mb{sort_bits}"
-    log_dir = base_logs_dir / f"{time}_{model_name}_{sort_suffix}_{rand_num}"
+    note = config.get("note", "N/A")
+    log_dir = base_logs_dir / f"{model_name}_{sort_suffix}_{note}_{rand_num}_{time}"
     log(f"Log dir: {log_dir}")
     log_dir.mkdir(parents=True, exist_ok=False)
     # save console log to file in log_dir, filtering out tqdm progress bar lines
